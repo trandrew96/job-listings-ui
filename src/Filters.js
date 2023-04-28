6becb6683571c4baa2a5acccc0ef1c69fe94f4ce
Andrew T <trandrew96@gmail.com>
2023-04-27 21:39:34 -0700

    improved filter layout on mobile

--- src/Filters.js
+++ src/Filters.js
@@ -6,60 +6,68 @@ export default function Filters({ filters, removeFilter, clearFilters }) {
   }
 
   return (
-    <div className="bg-white flex flex-wrap h-full px-5 mb-5">
-      {/* Level Filter */}
-      {filters.level && (
-        <div className="flex bg-lightGrayishCyan my-4 mr-3 rounded">
-          <div className="px-2 text-darkCyan">{filters.level}</div>
-          <button
-            onClick={() => {
-              removeFilter({ type: "change_level", level: "" });
-            }}
-            className="bg-darkCyan bg-remove-icon bg-no-repeat w-6 h-6 bg-center"
-          ></button>
-        </div>
-      )}
-
-      {/* Role Filter */}
-      {filters.role && (
-        <div className="flex bg-lightGrayishCyan my-4 mr-3 rounded">
-          <div className="px-2 text-darkCyan">{filters.role}</div>
-          <button
-            onClick={() => {
-              removeFilter({ type: "change_role", role: "" });
-            }}
-            className="bg-darkCyan bg-remove-icon bg-no-repeat w-6 h-6 bg-center"
-          ></button>
-        </div>
-      )}
-
-      {/* Languages Filter */}
-      {filters.languages.map((language) => {
-        return (
-          <div
-            className="flex bg-lightGrayishCyan my-4 mr-3 rounded"
-            key={language}
-          >
-            <div className="px-2 text-darkCyan">{language}</div>
+    <div className="flex w-full bg-white h-full px-5 mb-10 md:mb-5">
+      <div className="flex flex-wrap">
+        {/* Level Filter */}
+        {filters.level && (
+          <div className="flex bg-lightGrayishCyan my-4 mr-3 rounded">
+            <div className="px-2 text-darkCyan">{filters.level}</div>
             <button
               onClick={() => {
-                removeFilter({ type: "remove_language", language: language });
+                removeFilter({ type: "change_level", level: "" });
               }}
               className="bg-darkCyan bg-remove-icon bg-no-repeat w-6 h-6 bg-center"
             ></button>
           </div>
-        );
-      })}
+        )}
 
-      {/* Clear */}
-      {(filters.level || filters.role || filters.languages.length > 0) && (
-        <button
-          className="ml-auto text-darkCyan underline"
-          onClick={() => clearFilters({ type: "clear_filters" })}
-        >
-          Clear
-        </button>
-      )}
+        {/* Role Filter */}
+        {filters.role && (
+          <div className="flex bg-lightGrayishCyan my-4 mr-3 rounded">
+            <div className="px-2 text-darkCyan">{filters.role}</div>
+            <button
+              onClick={() => {
+                removeFilter({ type: "change_role", role: "" });
+              }}
+              className="bg-darkCyan bg-remove-icon bg-no-repeat w-6 h-6 bg-center"
+            ></button>
+          </div>
+        )}
+
+        {/* Languages Filter */}
+        {filters.languages.map((language) => {
+          return (
+            <div
+              className="flex bg-lightGrayishCyan my-4 mr-3 rounded"
+              key={language}
+            >
+              <div className="px-2 text-darkCyan">{language}</div>
+              <button
+                onClick={() => {
+                  removeFilter({
+                    type: "remove_language",
+                    language: language,
+                  });
+                }}
+                className="bg-darkCyan bg-remove-icon bg-no-repeat w-6 h-6 bg-center"
+              ></button>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="flex items-center ml-auto">
+        {" "}
+        {/* Clear */}
+        {(filters.level || filters.role || filters.languages.length > 0) && (
+          <button
+            className="text-darkGrayishCyan hover:text-darkCyan hover:underline"
+            onClick={() => clearFilters({ type: "clear_filters" })}
+          >
+            Clear
+          </button>
+        )}
+      </div>
     </div>
   );
 }
